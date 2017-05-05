6b0918dc0a723184cda43a409c0bfe6fef9b0a25
Fernando Cejas <fcejas@gmail.com>
2017-05-05 15:52:35 +0200

    Clean dependencies.

--- frodo2-runtime-java/src/main/java/com/fernandocejas/frodo2/java/JavaLog.java
+++ frodo2-runtime-java/src/main/java/com/fernandocejas/frodo2/java/JavaLog.java
@@ -2,13 +2,12 @@ package com.fernandocejas.frodo2.java;
 
 import com.fernandocejas.frodo2.logging.Logger;
 
-public class JavaLog implements Logger {
+class JavaLog implements Logger {
 
-  public JavaLog() {}
+  JavaLog() {}
 
   @Override
   public void log(String tag, String message) {
-    //TODO: implement proper logging
     System.out.println(message);
   }
 }
