29cf80dd513d333c4e35c419c6746250d5dcb6f6
Fernando Cejas <fcejas@gmail.com>
2017-10-25 17:23:53 +0200

    We need code style rules.

--- frodo2-runtime-android/src/test/java/com/fernandocejas/frodo2/android/ApplicationStub.java
+++ frodo2-runtime-android/src/test/java/com/fernandocejas/frodo2/android/ApplicationStub.java
@@ -2,6 +2,5 @@ package com.fernandocejas.frodo2.android;
 
 import android.app.Application;
 
-public class ApplicationStub extends Application {
-}
+public class ApplicationStub extends Application {}
 
