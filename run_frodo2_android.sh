8355c339dd35ec39b6db29277e8dad5cf98baf40
Arnaud Frugier <frugier.arnaud@gmail.com>
2017-10-27 09:43:09 -0700

    Fixes rename of scripts

--- run_frodo2_android.sh
+++ run_frodo2_android.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./install_frodo2.sh
-./gradlew clean -p frodo2-android-sample/ assembleDebug installDebug
+./gradlew clean -p frodo2-sample-android assembleDebug installDebug
 adb shell am start -n com.fernandocejas.example.frodo2.debug/com.fernandocejas.example.frodo2.MainActivity
