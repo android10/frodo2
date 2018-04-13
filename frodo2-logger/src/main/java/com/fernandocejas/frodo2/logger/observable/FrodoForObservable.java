566f66f3b20bdd2db565d7a01355c919f29202da
Arnaud Frugier <frugier.arnaud@gmail.com>
2018-04-13 14:13:06 -0700

    Fixes FrodoForObservable so that we can know which the Observable observeOn. It does not seem possible to figure out the subscribeOn thread

--- frodo2-logger/src/main/java/com/fernandocejas/frodo2/logger/observable/FrodoForObservable.java
+++ frodo2-logger/src/main/java/com/fernandocejas/frodo2/logger/observable/FrodoForObservable.java
@@ -6,13 +6,15 @@ import com.fernandocejas.frodo2.logger.joinpoint.RxComponentInfo;
 import com.fernandocejas.frodo2.logger.logging.Counter;
 import com.fernandocejas.frodo2.logger.logging.MessageManager;
 import com.fernandocejas.frodo2.logger.logging.StopWatch;
+
 import io.reactivex.Notification;
 import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 
-@SuppressWarnings({ "unchecked", "Convert2Lambda" }) class FrodoForObservable {
+@SuppressWarnings({"unchecked", "Convert2Lambda"})
+class FrodoForObservable {
 
   private final FrodoProceedingJoinPoint joinPoint;
   private final MessageManager messageManager;
@@ -34,53 +36,60 @@ import io.reactivex.functions.Consumer;
     final StopWatch stopWatch = new StopWatch();
     final Counter emittedItems = new Counter(joinPoint.getMethodName());
     return ((Observable<T>) joinPoint.proceed())
-        .doOnSubscribe(new Consumer<Disposable>() {
-          @Override public void accept(Disposable disposable) throws Exception {
-            stopWatch.start();
-            messageManager.printOnSubscribe(rxComponentInfo);
-          }
-        })
-        .doOnEach(new Consumer<Notification<T>>() {
-          @Override public void accept(Notification<T> notification) throws Exception {
-            if (rxComponentInfo.subscribeOnThread() != null && (notification.isOnNext()
-                || notification.isOnError())) {
-              rxComponentInfo.setSubscribeOnThread(Thread.currentThread().getName());
-            }
-          }
-        })
-        .doOnNext(new Consumer<T>() {
-          @Override public void accept(T value) throws Exception {
-            emittedItems.increment();
-            messageManager.printOnNextWithValue(rxComponentInfo, value);
-          }
-        })
-        .doOnError(new Consumer<Throwable>() {
-          @Override public void accept(Throwable throwable) throws Exception {
-            messageManager.printOnError(rxComponentInfo, throwable);
-          }
-        })
-        .doOnComplete(new Action() {
-          @Override public void run() throws Exception {
-            messageManager.printOnCompleted(rxComponentInfo);
-          }
-        })
-        .doOnTerminate(new Action() {
-          @Override public void run() throws Exception {
-            stopWatch.stop();
-            rxComponentInfo.setTotalExecutionTime(stopWatch.getTotalTimeMillis());
-            rxComponentInfo.setTotalEmittedItems(emittedItems.tally());
-            messageManager.printOnTerminate(rxComponentInfo);
-            messageManager.printItemTimeInfo(rxComponentInfo);
-          }
-        })
-        .doFinally(new Action() {
-          @Override public void run() throws Exception {
-            if (rxComponentInfo.observeOnThread() != null) {
-              rxComponentInfo.setObserveOnThread(Thread.currentThread().getName());
-            }
-            messageManager.printThreadInfo(rxComponentInfo);
-            messageManager.printOnUnsubscribe(rxComponentInfo);
-          }
-        });
+            .doOnSubscribe(new Consumer<Disposable>() {
+              @Override
+              public void accept(Disposable disposable) throws Exception {
+                stopWatch.start();
+                messageManager.printOnSubscribe(rxComponentInfo);
+              }
+            })
+            .doOnEach(new Consumer<Notification<T>>() {
+              @Override
+              public void accept(Notification<T> notification) throws Exception {
+                if (rxComponentInfo.subscribeOnThread() == null && (notification.isOnNext()
+                        || notification.isOnError())) {
+                  rxComponentInfo.setSubscribeOnThread(Thread.currentThread().getName());
+                }
+              }
+            })
+            .doOnNext(new Consumer<T>() {
+              @Override
+              public void accept(T value) throws Exception {
+                emittedItems.increment();
+                messageManager.printOnNextWithValue(rxComponentInfo, value);
+              }
+            })
+            .doOnError(new Consumer<Throwable>() {
+              @Override
+              public void accept(Throwable throwable) throws Exception {
+                messageManager.printOnError(rxComponentInfo, throwable);
+              }
+            })
+            .doOnComplete(new Action() {
+              @Override
+              public void run() throws Exception {
+                messageManager.printOnCompleted(rxComponentInfo);
+              }
+            })
+            .doOnTerminate(new Action() {
+              @Override
+              public void run() throws Exception {
+                stopWatch.stop();
+                rxComponentInfo.setTotalExecutionTime(stopWatch.getTotalTimeMillis());
+                rxComponentInfo.setTotalEmittedItems(emittedItems.tally());
+                messageManager.printOnTerminate(rxComponentInfo);
+                messageManager.printItemTimeInfo(rxComponentInfo);
+              }
+            })
+            .doFinally(new Action() {
+              @Override
+              public void run() throws Exception {
+                if (rxComponentInfo.observeOnThread() == null) {
+                  rxComponentInfo.setObserveOnThread(Thread.currentThread().getName());
+                }
+                messageManager.printThreadInfo(rxComponentInfo);
+                messageManager.printOnUnsubscribe(rxComponentInfo);
+              }
+            });
   }
 }
